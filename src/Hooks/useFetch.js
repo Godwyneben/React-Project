b98b75140e894cfa9732ed6102ec13eb85902f3e
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-06-27 02:55:05 +0100

    final commit

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -7,11 +7,14 @@ const useFetch = (url) => {
 
 
     useEffect(() => {
+
+        const controller = new AbortController
+
         const fetchData = async () => {
             setIsPending(true)
 
             try{
-                const res = await fetch(url)
+                const res = await fetch(url, {signal: controller.signal})
                 if (!res.ok){
                     throw new Error(res.statusText)
                 }
@@ -21,14 +24,21 @@ const useFetch = (url) => {
                 setData(json)
                 setError(null)
             } catch (err){
-                setIsPending(false)
-                setError('Could not fetch data')
-                console.log(err.message)
+                if (err.name === "AbortError") {
+                    console.log('the fetch was aborted')
+                } else{
+                    setIsPending(false)
+                    setError('Could not fetch data')
+                }
             }
 
         }
 
         fetchData()
+
+        return () => {
+            controller.abort()
+        }
     }, [url])
 
     return { data, isPending, error }
