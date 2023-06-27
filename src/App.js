bad814388f00349c91fd04572e3f737c71518dcd
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-06-27 02:31:26 +0100

    app updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import TripLists from './Components/TripLists';
 
 export default function App() {
+  const [showTrips, setShowTrips] = useState(true)
+
   return (
     <div className='App'>
-      <TripLists />
+      <button onClick={() => setShowTrips(false)}>Hide trips</button>
+      {showTrips && <TripLists />}
     </div>
   )
 }
